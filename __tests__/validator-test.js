facbfa8e8870e0337bcce7cc79fb5817e0c3dc26
CC32421 <heidi.munthe-kaas@consult.nordea.no>
2016-09-16 17:25:52 +0200

    Added tests to validate userId as input

--- __tests__/validator-test.js
+++ __tests__/validator-test.js
@@ -1,7 +1,18 @@
 
 'use strict';
 
-test('Check validateUserId', () => {
-  const validator = require("../validator.js");
+const validator = require("../validator.js");
+
+test('validateUserId should return true for correct input', () => {
 expect(validator.validateUserId("AC32145")).toBe(true);
+});
+
+
+test('validateUserId should return false for input with wrong first letters', () => {
+expect(validator.validateUserId("AA32421")).toBe(false);
+});
+
+
+test('validateUserId should return false for incorrect number if digits', () => {
+  expect(validator.validateUserId("CC3242")).toBe(false);
 });
