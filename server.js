ff59cfd69d6b3a3d921bd1d750bda84b58be5135
Fredrik Oterholt <fredrik.oterholt@consult.nordea.com>
2016-09-16 09:49:53 +0200

    Added .editorconfig.

--- server.js
+++ server.js
@@ -15,20 +15,20 @@ var db;
 
 // Connect to the database before starting the application server.
 mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
-    if (err) {
-        console.log(err);
-        process.exit(1);
-    }
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
 
-    // Save database object from the callback for reuse.
-    db = database;
-    console.log("Database connection ready");
+  // Save database object from the callback for reuse.
+  db = database;
+  console.log("Database connection ready");
 
-    // Initialize the app.
-    var server = app.listen(process.env.PORT || 8080, function () {
-        var port = server.address().port;
-        console.log("App now running on port", port);
-    });
+  // Initialize the app.
+  var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+  });
 });
 
 // CONTACTS API ROUTES BELOW
